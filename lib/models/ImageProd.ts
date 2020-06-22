f1a308e54e16fc13d131adbd9bc5193e5ccd9911
endorphin82 <endorphin82@gmail.com>
2020-06-22 18:32:27 +0300

    format

--- lib/models/ImageProd.ts
+++ lib/models/ImageProd.ts
@@ -1,12 +1,12 @@
-import {objectType} from '@nexus/schema'
+import { objectType } from '@nexus/schema'
 
 export const ImageProd = objectType({
-    name: 'ImageProd',
-    definition(t) {
-        t.model.id()
-        t.model.product()
-        t.model.url()
-        t.model.product_id()
-        t.upload("uploadFile")
-    }
+  name: 'ImageProd',
+  definition(t) {
+    t.model.id()
+    t.model.product()
+    t.model.url()
+    t.model.product_id()
+    t.upload('uploadFile')
+  }
 })
