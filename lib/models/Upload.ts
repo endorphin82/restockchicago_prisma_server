66dfb723c1f0686ca9534c203299e3887b99571d
endorphin82 <endorphin82@gmail.com>
2020-06-22 17:36:33 +0300

    t.upload

--- lib/models/Upload.ts
+++ lib/models/Upload.ts
@@ -1,18 +1,18 @@
-// import { scalarType } from '@nexus/schema';
+import { scalarType } from '@nexus/schema';
 import { GraphQLUpload } from 'graphql-upload';
 
-export const Upload = GraphQLUpload;
+// export const Upload = GraphQLUpload;
 // export const Upload = scalarType({
 //   name: "Upload",
 //   serialize() { /* Todo */ },
 //   parseValue() { /* Todo */ },
 //   parseLiteral() { /* Todo */ }
 // });
-// export const Upload = scalarType({
-//   name: GraphQLUpload.name,
-//   asNexusMethod: 'upload', // We set this to be used as a method later as `t.upload()` if needed
-//   // description: GraphQLUpload.description,
-//   serialize: GraphQLUpload.serialize,
-//   parseValue: GraphQLUpload.parseValue,
-//   parseLiteral: GraphQLUpload.parseLiteral,
-// })
+export const Upload = scalarType({
+  name: GraphQLUpload.name,
+  asNexusMethod: 'upload', // We set this to be used as a method later as `t.upload()` if needed
+  // description: GraphQLUpload.description,
+  serialize: GraphQLUpload.serialize,
+  parseValue: GraphQLUpload.parseValue,
+  parseLiteral: GraphQLUpload.parseLiteral,
+})
