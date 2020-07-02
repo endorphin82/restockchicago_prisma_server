e1cbda8b2fa7ac7df7738bb2b8065758eb7d7315
endorphin82 <endorphin82@gmail.com>
2020-07-02 19:15:52 +0300

    process extend mutation

--- lib/models/Product.ts
+++ lib/models/Product.ts
@@ -1,4 +1,7 @@
-import { objectType } from '@nexus/schema'
+import { arg, extendType, inputObjectType, intArg, interfaceType, objectType, mutationField } from '@nexus/schema'
+import { Upload } from './Upload'
+import fs from 'fs'
+import path from 'path'
 
 export const Product = objectType({
   name: 'Product',
@@ -11,5 +14,60 @@ export const Product = objectType({
     t.model.icon()
     t.model.price()
     t.model.images()
+    // t.upload('up', { list: true })
+    // t.field('files', { type: Upload, list: true })
+    // t.list.field('files', { type: Upload })
   }
+})
+
+export const ProductAndUpload = mutationField((t) => {
+  t.connectionField('UploadFiles', {
+    type: 'File',
+    list: true,
+    args: {
+      files: arg({ type: 'Upload', nullable: false, list: true }),
+      product_id: intArg({ required: false })
+    },
+    resolve: async (parent, args) => {
+      const { files } = await args
+      const resFiles = Promise.all(
+        files
+          .map(
+            async (file) => {
+              const { createReadStream, filename } = await file
+              if (!filename) {
+                throw Error('Invalid file Stream')
+              } else if (filename) {
+                const readStream = createReadStream(filename)
+                const newName = `${args.product_id}_${filename}`
+                readStream
+                  .pipe(fs.createWriteStream(path.join(__dirname, '../../uploads/', newName)))
+                  .on('close', () => {
+                    console.log('onClose')
+                  })
+                return {
+                  uid: String(Date.now()),
+                  name: newName,
+                  status: 'done',
+                  url: `www.${newName}`
+                }
+              }
+            }
+          )
+      )
+      // console.log('resFiles', resFiles)
+      console.log('------', resFiles)
+      return resFiles.then(res => {
+        console.log('res---', res)
+        return res
+      }).catch(err => {
+        console.log('err---', err)
+        return err
+      })
+    }
+  })
+  // t.upload('up', { list: true })
+  // t.field('files', { type: Upload, list: true })
+  // t.list.field('files', { type: Upload })
+
 })
